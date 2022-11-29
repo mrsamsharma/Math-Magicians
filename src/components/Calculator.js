7fd187bef018c70cdcb0d67d8e2c029678af92a8
Satyam Sharma <samwfelice@gmail.com>
2022-11-29 09:40:58 -0800

    Add styles.css

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import '../styles.css';
 // eslint-disable-next-line react/prefer-stateless-function
 class Calculator extends React.Component {
   render() {
@@ -11,19 +11,19 @@ class Calculator extends React.Component {
         <button type="button">AC</button>
         <button type="button">+/-</button>
         <button type="button">%</button>
-        <button type="button">÷</button>
+        <button className="orange" type="button">÷</button>
         <button type="button">7</button>
         <button type="button">8</button>
         <button type="button">9</button>
-        <button type="button">X</button>
+        <button className="orange" type="button">x</button>
         <button type="button">4</button>
         <button type="button">5</button>
         <button type="button">6</button>
-        <button type="button">-</button>
+        <button className="orange" type="button">-</button>
         <button type="button">1</button>
         <button type="button">2</button>
         <button type="button">3</button>
-        <button type="button">+</button>
+        <button className="orange" type="button">+</button>
         <button type="button">0</button>
         <button type="button">.</button>
         <button type="button" className="span-two">=</button>
