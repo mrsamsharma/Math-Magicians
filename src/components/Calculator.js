a169bd1d79c10a54811dece1daa4a29ff100b6d5
Satyam Sharma <samwfelice@gmail.com>
2022-11-29 09:28:32 -0800

    Fix syntax typos and erros

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,28 +1,35 @@
-const Calculator = () => (
-  <div className="calculator-grid">
-    <div className="output">
-      <div className="operand">0</div>
-    </div>
-    <button type="button">AC</button>
-    <button type="button">+/-</button>
-    <button type="button">%</button>
-    <button type="button">÷</button>
-    <button type="button">7</button>
-    <button type="button">8</button>
-    <button type="button">9</button>
-    <button type="button">X</button>
-    <button type="button">4</button>
-    <button type="button">5</button>
-    <button type="button">6</button>
-    <button type="button">-</button>
-    <button type="button">1</button>
-    <button type="button">2</button>
-    <button type="button">3</button>
-    <button type="button">+</button>
-    <button type="button">0</button>
-    <button type="button">.</button>
-    <button type="button" className="span-two">=</button>
-  </div>
-);
+import React from 'react';
+
+// eslint-disable-next-line react/prefer-stateless-function
+class Calculator extends React.Component {
+  render() {
+    return (
+      <div className="calculator-grid">
+        <div className="output">
+          <div className="operand">0</div>
+        </div>
+        <button type="button">AC</button>
+        <button type="button">+/-</button>
+        <button type="button">%</button>
+        <button type="button">÷</button>
+        <button type="button">7</button>
+        <button type="button">8</button>
+        <button type="button">9</button>
+        <button type="button">X</button>
+        <button type="button">4</button>
+        <button type="button">5</button>
+        <button type="button">6</button>
+        <button type="button">-</button>
+        <button type="button">1</button>
+        <button type="button">2</button>
+        <button type="button">3</button>
+        <button type="button">+</button>
+        <button type="button">0</button>
+        <button type="button">.</button>
+        <button type="button" className="span-two">=</button>
+      </div>
+    );
+  }
+}
 
 export default Calculator;
