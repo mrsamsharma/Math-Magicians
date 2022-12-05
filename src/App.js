52e98fc812c42ffbe8787d7cb7ce2fdb228ad2fe
Satyam Sharma <samwfelice@gmail.com>
2022-12-05 10:15:57 -0800

    Design the UI of Calculator Page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import Calculator from './components/Calculator';
+import Navbar from './components/Navbar';
 
 // eslint-disable-next-line react/prefer-stateless-function
 class App extends React.Component {
   render() {
     return (
-      <Calculator />
+      <>
+        <Navbar />
+        <Calculator />
+      </>
     );
   }
 }
--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -16,34 +16,37 @@ function Calculator() {
 
   const { total, next, operation } = data;
   return (
-    <div className="calculator-grid">
-      <div className="output">
-        <div className="operand">
-          {total}
-          {operation}
-          {next}
+    <>
+      <h1 className="heading">Let&apos;s do some Maths!</h1>
+      <div className="calculator-grid">
+        <div className="output">
+          <div className="operand">
+            {total}
+            {operation}
+            {next}
+          </div>
         </div>
+        <button type="button" onClick={eventHandle}>AC</button>
+        <button type="button" onClick={eventHandle}>+/-</button>
+        <button type="button" onClick={eventHandle}>%</button>
+        <button className="orange" type="button" onClick={eventHandle}>÷</button>
+        <button type="button" onClick={eventHandle}>7</button>
+        <button type="button" onClick={eventHandle}>8</button>
+        <button type="button" onClick={eventHandle}>9</button>
+        <button className="orange" type="button" onClick={eventHandle}>x</button>
+        <button type="button" onClick={eventHandle}>4</button>
+        <button type="button" onClick={eventHandle}>5</button>
+        <button type="button" onClick={eventHandle}>6</button>
+        <button className="orange" type="button" onClick={eventHandle}>-</button>
+        <button type="button" onClick={eventHandle}>1</button>
+        <button type="button" onClick={eventHandle}>2</button>
+        <button type="button" onClick={eventHandle}>3</button>
+        <button className="orange" type="button" onClick={eventHandle}>+</button>
+        <button type="button" onClick={eventHandle}>0</button>
+        <button type="button" onClick={eventHandle}>.</button>
+        <button type="button" className="span-two" onClick={eventHandle}>=</button>
       </div>
-      <button type="button" onClick={eventHandle}>AC</button>
-      <button type="button" onClick={eventHandle}>+/-</button>
-      <button type="button" onClick={eventHandle}>%</button>
-      <button className="orange" type="button" onClick={eventHandle}>÷</button>
-      <button type="button" onClick={eventHandle}>7</button>
-      <button type="button" onClick={eventHandle}>8</button>
-      <button type="button" onClick={eventHandle}>9</button>
-      <button className="orange" type="button" onClick={eventHandle}>x</button>
-      <button type="button" onClick={eventHandle}>4</button>
-      <button type="button" onClick={eventHandle}>5</button>
-      <button type="button" onClick={eventHandle}>6</button>
-      <button className="orange" type="button" onClick={eventHandle}>-</button>
-      <button type="button" onClick={eventHandle}>1</button>
-      <button type="button" onClick={eventHandle}>2</button>
-      <button type="button" onClick={eventHandle}>3</button>
-      <button className="orange" type="button" onClick={eventHandle}>+</button>
-      <button type="button" onClick={eventHandle}>0</button>
-      <button type="button" onClick={eventHandle}>.</button>
-      <button type="button" className="span-two" onClick={eventHandle}>=</button>
-    </div>
+    </>
   );
 }
 
