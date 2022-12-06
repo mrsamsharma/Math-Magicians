b001de3c7ddea4b3863d567486d1b1463c585024
Satyam Sharma <samwfelice@gmail.com>
2022-12-06 00:15:56 -0800

    Create Unit test for Calculate.js

--- src/logic/operate.test.js
+++ src/logic/operate.test.js
@@ -7,20 +7,8 @@ const two = Big(2);
 
 test('1 + 2 equals 3', () =>{
     expect(operate(one, two, '+')).toEqual('3');
-});
-
-test('1 - 2 equals -1', () =>{
-    expect(operate(one, two, '-')).toEqual('-1');
-});
-
-test('1 x 2 equals 2', () =>{
+    expect(operate(one, two, '%')).toEqual('1');
     expect(operate(one, two, 'x')).toEqual('2');
-});
-
-test('1 ÷ 2 equals 0.5', () =>{
     expect(operate(one, two, '÷')).toEqual('0.5');
-});
-
-test('1 % 2 equals 1', () =>{
     expect(operate(one, two, '%')).toEqual('1');
 });
